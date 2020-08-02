612e388d1182e1a248a450f0e85cf56cf34bf785
Ernesto Resende <ernestoresende@hotmail.com>
2020-08-02 20:12:42 -0300

    course project: 13. Going back with NavController

--- course-project/src/app/places/discover/place-detail/place-detail.page.ts
+++ course-project/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    /* NavController ensures that when the "Book" button is clicked, 
+    the user gets back to the last page on the stack, instead of using
+    Angular's own routing solution. */
+
+    // private router: Router
+    private navCtrl: NavController) {}
 
   ngOnInit() {
+  }
+
+  onBookPlace() {
+    // this.router.navigateByUrl('/places/tabs/discover')
+    this.navCtrl.navigateBack('places/tabs/discover')
+    this.navCtrl.pop()
+    /* We could also use the pop method, but it's unreliable if you can't garantee
+    the existence of the page it should be pointing to in that context. */
   }
 
 }
